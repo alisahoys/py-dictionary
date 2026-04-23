fefa82c5bc32a3c4a7ea2ba4efe32a4dab4eb1ce
Alisa Hoys <hoys@protonmail.com>
2026-04-23 11:45:57 +0200

    Corrected the implementation of compulsory methods of a dictionary class

--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ class Node:
     def __init__(self, key: Any, value: Any) -> None:
         self.value = value
         self.key = key
+        self.hash = hash(key)
 
 
 class Dictionary:
@@ -14,29 +15,33 @@ class Dictionary:
         self.storage = [None] * self.capacity
 
     def __setitem__(self, key: Any, value: Any) -> None:
-        index = hash(key) % self.capacity
+        key_hash = hash(key)
+        index = key_hash % self.capacity
         if self.storage[index] is None:
             self.storage[index] = [Node(key, value)]
             self.size += 1
             return
+
         for node in self.storage[index]:
-            if node.key == key:
+            if node.hash == key_hash and node.key == key:
                 node.value = value
                 return
+
         self.storage[index].append(Node(key, value))
         self.size += 1
         if self.size / self.capacity > 0.75:
             self.resize()
 
     def __getitem__(self, key: Any) -> Any:
-        index = hash(key) % self.capacity
+        key_hash = hash(key)
+        index = key_hash % self.capacity
         bucket = self.storage[index]
         if bucket is not None:
             for node in bucket:
-                if node.key == key:
+                if node.hash == key_hash and node.key == key:
                     return node.value
 
-        raise KeyError(key)
+        raise KeyError(f"Key '{key}' not found")
 
     def __len__(self) -> int:
         return self.size
